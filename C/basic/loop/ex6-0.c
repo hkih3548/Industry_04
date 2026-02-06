04d684c750a40a3f119d9283674f1d4bb5a626f5
hkih3548 <hkiho97@naver.com>
2026-02-06 15:12:28 +0900

    Create ex6-0.c

---
+++ C/basic/loop/ex6-0.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+int main()
+{
+    //기수값 index
+    int i = 0; //index
+
+    while (i < 10) //조건 만들기
+    {
+        printf("%d\n", i);
+        i++; //기수값의 증가감
+    }
+
+
+    return 0;
+}
